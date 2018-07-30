ee631232ce5ec57e14542b385abc6c97da0eb882
Fabian Paul <fpaul@mesosphere.io>
2018-07-30 16:59:48 +0200

    Always deploy latest docker version

--- deploy.py
+++ deploy.py
@@ -39,7 +39,7 @@ def main(dcos_username, dcos_password, script_cpus, script_mem, topic,
         "container": {
             "type": "DOCKER",
             "docker": {
-                "image": "fpaul/dcos-kafka-load-test",
+                "image": "fpaul/dcos-kafka-load-test:latest",
             }
         },
         "cpus": script_cpus,
